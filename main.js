86b6b1bf95118d48f2ce9cc4b32673967c7b9c94
CaptainSkippy <86449073+CaptainSkippy@users.noreply.github.com>
2021-07-28 14:14:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,22 @@
+function pullRandomWord(array){
+    let word = array[Math.floor(Math.random() * (array.length))];
+    return word;
+};
+
 function generateSentence(){
     var idea, templateSeed;
 
-    templateSeed = Math.floor(Math.random() * 6);
+    templateSeed = Math.floor(Math.random() * 1);
 
     switch(templateSeed){
         case 0:
-            //a MOOD GENRE/GENRE game base around THEME in an SCENE
-            idea = moodStart[Math.floor(Math.random() * (moodStart.length))] + " " + genre[Math.floor(Math.random() * (genre.length))] + "/" + genre[Math.floor(Math.random() * (genre.length))] + " game based around " + theme[Math.floor(Math.random() * (theme.length))] + " in " + scene[Math.floor(Math.random() * (scene.length))];
-            break;
-        case 1:
-            //a MOOD GENRE game about THEME
-            idea = moodStart[Math.floor(Math.random() * (moodStart.length))] + " " + genre[Math.floor(Math.random() * (genre.length))] + " game about " + theme[Math.floor(Math.random() * (theme.length))];
-            break;
-        case 2:
-            //a MOOD GENRE game where you play as AGENERALCHARACTER
-            idea = moodStart[Math.floor(Math.random() * (moodStart.length))] + " " + genre[Math.floor(Math.random() * (genre.length))] + " game where you play as " + aGeneralCharacter[Math.floor(Math.random() * (aGeneralCharacter.length))];
-            break;
-        case 3:
-            //a MOOD GENRE game where you play as a CHARACTERDESCRIPTION GENERALCHARACTER
-            idea = moodStart[Math.floor(Math.random() * (moodStart.length))] + " " + genre[Math.floor(Math.random() * (genre.length))] + " game where you play as a " + characterDescriptions[Math.floor(Math.random() * (characterDescriptions.length))] + " " + generalCharacter[Math.floor(Math.random() * (generalCharacter.length))];
-            break;
-        case 4:
-            //a MOOD GENRE game where you play as a GROUPNAMES of GENERALCHARACTERS
-            idea = moodStart[Math.floor(Math.random() * (moodStart.length))] + " " + genre[Math.floor(Math.random() * (genre.length))] + " game where you play as a " + groupNames[Math.floor(Math.random() * (groupNames.length))] + " of "+ generalCharacters[Math.floor(Math.random() * (generalCharacters.length))];
-            break;
-        case 5:
-            //a MOOD GENRE game where you play as a GROUPNAMES of CHARACTERDESCRIPTION GENERALCHARACTERS
-            idea = moodStart[Math.floor(Math.random() * (moodStart.length))] + " " + genre[Math.floor(Math.random() * (genre.length))] + " game where you play as a " + groupNames[Math.floor(Math.random() * (groupNames.length))] + " of " + characterDescriptions[Math.floor(Math.random() * (characterDescriptions.length))] + " " + generalCharacters[Math.floor(Math.random() * (generalCharacters.length))];
+            //A MOOD GENRE game based in AINSETTING where you play as a GENERALCHARACTER trying to PEOPLEOBJECTIVES a GROUPNAMES of GENERALCHARACTERS
+            idea = pullRandomWord(moodStart) + " " + pullRandomWord(genre) + " game based in " + pullRandomWord(aInSetting) + " where you play as " + pullRandomWord(aGeneralCharacter) + " trying to " + pullRandomWord(peopleObjectives) + " a " + pullRandomWord(groupNames) + " of " + pullRandomWord(generalCharacters);
             break;
     }
+
+    //include: SETTING, MOOD, GENRE, CHARACTER, OBJECTIVES
+    //Optional: ART-STYLE, ENEMY(If not part of objective), 
 
     document.getElementById("gameIdeaArea").innerHTML = idea;
 };
